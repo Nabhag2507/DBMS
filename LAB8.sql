9b40931d8aca8cf59162e77af55aa8953d5ff7d9
Nabhag2507 <nabhagsorathiya81@gmail.com>
2025-07-10 11:29:47 +0530

    lab 7 Perform SQL queries for Aggerate function and group by (without having) completed on 7-10-2025

---
+++ LAB8.sql
@@ -0,0 +1,22 @@
+USE CSE_3A_261;
+
+CREATE TABLE SalesData (
+    Region VARCHAR(50),
+    Product VARCHAR(50),
+    Sales_Amount INT,
+    Year INT
+);
+
+INSERT INTO SalesData (Region, Product, Sales_Amount, Year) VALUES
+('North America', 'Watch', 1500, 2023),
+('Europe', 'Mobile', 1200, 2023),
+('Asia', 'Watch', 1800, 2023),
+('North America', 'TV', 900, 2024),
+('Europe', 'Watch', 2000, 2024),
+('Asia', 'Mobile', 1000, 2024),
+('North America', 'Mobile', 1600, 2023),
+('Europe', 'TV', 1500, 2023),
+('Asia', 'TV', 1100, 2024),
+('North America', 'Watch', 1700, 2024);
+
+SELECT * FROM SalesData;
