10edbb1432e86309fef1f1cf837afc87cdcdbac2
Nabhag2507 <nabhagsorathiya81@gmail.com>
2025-12-06 11:35:33 +0530

    first lab of DBMS-II completed in fourth semester

--- DBMS II/Lab 1.sql
+++ DBMS II/Lab 1.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE CSE_4B_450
 
-USE CSE_4B_450
+USE CSE_4B_465
 
 select * from students
 
@@ -96,16 +96,125 @@ SELECT UPPER(FACULTYNAME)
 FROM FACULTY
 
 --8.	Find all faculty who joined after 2015. (FACULTY table)
-
+SELECT FacultyName
+FROM FACULTY
+WHERE FacultyJoiningDate < '01-01-2015'
 
 --9.	Find the SQUARE ROOT of Credits for the course 'Database Management Systems'. (COURSE table)
+SELECT SQRT(COURSECREDITS)
+FROM COURSE
+WHERE CourseName = 'Database Management Systems'
+
 --10.	Find the Current Date using SQL Server in-built function.
+SELECT GETDATE()
+
 --11.	Find the top 3 students who enrolled earliest (by EnrollmentYear). (STUDENT table)
+SELECT TOP 3 StuName,StuEnrollmentYear
+FROM STUDENTS
+GROUP BY StuEnrollmentYear,StuName
+ORDER BY StuEnrollmentYear DESC
+
 --12.	Find all enrollments that were made in the year 2022. (ENROLLMENT table)
+SELECT EnrollmentID
+FROM ENROLLMENT
+WHERE EnrollmentDate BETWEEN '01-01-2022' AND '01-01-2023'
+
 --13.	Find the number of courses offered by each department. (COURSE table)
+SELECT COUNT(CourseID),CourseDepartment
+FROM COURSE
+GROUP BY CourseDepartment
+
 --14.	Retrieve the CourseID which has more than 2 enrollments. (ENROLLMENT table)
+SELECT COUNT(CourseID),CourseID
+FROM ENROLLMENT
+GROUP BY CourseID
+HAVING COUNT(CourseID) > 2
+
 --15.	Retrieve all the student name with their enrollment status. (STUDENT & ENROLLMENT table)
+SELECT StuName, EnrollmentID
+FROM STUDENTS JOIN ENROLLMENT
+ON STUDENTS.StudentID = ENROLLMENT.StudentID
+
 --16.	Select all student names with their enrolled course names. (STUDENT, COURSE, ENROLLMENT table)
+SELECT StuName, CourseName
+FROM STUDENTS JOIN ENROLLMENT
+ON STUDENTS.StudentID = ENROLLMENT.StudentID
+JOIN COURSE ON ENROLLMENT.CourseID = COURSE.CourseID
+
 --17.	Create a view called 'ActiveEnrollments' showing only active enrollments with student name and  course name. (STUDENT, COURSE, ENROLLMENT,  table)
---18.	Retrieve the student’s name who is not enrol in any course using subquery. (STUDENT, ENROLLMENT TABLE)
+CREATE VIEW ActiveEnrollments
+AS
+SELECT StuName, CourseName
+FROM STUDENTS JOIN ENROLLMENT
+ON STUDENTS.StudentID = ENROLLMENT.StudentID
+JOIN COURSE ON ENROLLMENT.CourseID = COURSE.CourseID
+
+SELECT * FROM ActiveEnrollments
+
+--18.	Retrieve the studentï¿½s name who is not enrol in any course using subquery. (STUDENT, ENROLLMENT TABLE)
+SELECT STUNAME
+FROM STUDENTS
+WHERE StudentID NOT IN(
+	SELECT StudentID FROM ENROLLMENT
+)
+
+--without subquery
+SELECT STUNAME,STUDENTS.StudentID,ENROLLMENT.StudentID
+FROM STUDENTS LEFT JOIN ENROLLMENT
+ON STUDENTS.StudentID = ENROLLMENT.StudentID
+WHERE ENROLLMENT.StudentID IS NULL
+
 --19.	Display course name having second highest credit. (COURSE table)
+SELECT TOP 1 CourseName 
+FROM COURSE
+WHERE CourseCredits = (
+	SELECT TOP 2 CourseCredits
+	ORDER BY CourseCredits DESC
+)
+
+--Part ï¿½ B 
+
+--20.	Retrieve all courses along with the total number of students enrolled. (COURSE, ENROLLMENT table)
+SELECT COUNT(EnrollmentID),CourseName
+FROM COURSE JOIN ENROLLMENT
+ON COURSE.CourseID = ENROLLMENT.CourseID
+GROUP BY CourseName
+
+--21.	Retrieve the total number of enrollments for each status, showing only statuses that have more than 2 enrollments. (ENROLLMENT table)
+SELECT COUNT(EnrollmentID),EnrollmentStatus
+FROM ENROLLMENT
+GROUP BY EnrollmentStatus
+HAVING COUNT(EnrollmentID) >= 2
+
+--22.	Retrieve all courses taught by 'Dr. Sheth' and order them by Credits. (FACULTY, COURSE, COURSE_ASSIGNMENT table)
+SELECT CourseName,CourseCredits
+FROM COURSE JOIN COURSE_ASSIGNMENT
+ON COURSE.CourseID = COURSE_ASSIGNMENT.CourseID
+JOIN FACULTY ON FACULTY.FacultyID = COURSE_ASSIGNMENT.FacultyID
+WHERE FACULTY.FacultyName = 'Dr. Sheth'
+ORDER BY CourseCredits
+
+--Part ï¿½ C 
+
+--23.	List all students who are enrolled in more than 3 courses. (STUDENT, ENROLLMENT table)
+SELECT StuName,COUNT(EnrollmentID)
+FROM STUDENTS JOIN ENROLLMENT
+ON STUDENTS.StudentID = ENROLLMENT.StudentID
+GROUP BY StuName
+HAVING COUNT(ENROLLMENTID) > 3
+
+--24.	Find students who have enrolled in both 'CS101' and 'CS201' Using Sub Query. (STUDENT, ENROLLMENT table)
+SELECT StuName
+FROM STUDENTS
+WHERE StudentID IN (
+	SELECT StudentID FROM ENROLLMENT
+	WHERE CourseID IN ('CS101','CS201')
+)
+
+--25.	Retrieve department-wise count of faculty members along with their average years of experience (calculate experience from JoiningDate). (Faculty table)
+SELECT
+	FacultyDepartment AS Department,
+	COUNT(*) AS FacultyCount,
+	ROUND(AVG(CAST(DATEDIFF(YEAR, FacultyJoiningDate, GETDATE()) AS FLOAT)), 2) AS AvgYearsExperience
+FROM FACULTY
+GROUP BY FacultyDepartment;
